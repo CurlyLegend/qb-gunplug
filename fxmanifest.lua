be3227e4ffb3deba769d8f898038596d68cd2b93
CurlyLegend <92329556+CurlyLegend@users.noreply.github.com>
2022-05-07 11:45:54 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'qb-gunplug'
+Author 'Curly Legend'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
